e6f1122c96356b65bdb6871435377012b0c07974
Bharat Ramnani <bharat.ramnani@hdfcred.com>
2017-12-13 14:07:43 +0530

    Add Sort functionality

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function githubJobApi() {
 }
 
 
-function createElements(url, logo, name, title, type) {
+function createElements(url, logo, name, title, location, type) {
     // debugger;
     var company_card = document.createElement('a');
     var company_detail = document.createElement('div');
@@ -28,6 +28,7 @@ function createElements(url, logo, name, title, type) {
     var company_logo = document.createElement('img');
     var company_name = document.createElement('p');
     var job_title = document.createElement('p');
+    var job_location = document.createElement('p');
     var job_type = document.createElement('p');
 
     company_card.append(company_detail);
@@ -37,6 +38,7 @@ function createElements(url, logo, name, title, type) {
     company_detail.append(company_name);
 
     job_detail.append(job_title);
+    job_detail.append(job_location);
     job_detail.append(job_type);
 
     company_card.className = "company_card";
@@ -45,12 +47,14 @@ function createElements(url, logo, name, title, type) {
     company_logo.className = "company_logo";
     company_name.className = "company_name";
     job_title.className = "job_title";
+    job_location.className = "job_location";
     job_type.className = "job_type";
 
     company_card.href = url;
     company_logo.src = logo;
     company_name.textContent = name;
     job_title.textContent = title;
+    job_location.textContent = location;
     job_type.textContent = type;
 
     return company_card;
@@ -59,16 +63,44 @@ function createElements(url, logo, name, title, type) {
 
 function addJobDetails(jobs) {
     // debugger;
+    sectionElement.innerHTML = '';
+
     for (var i = 0; i < jobs.length; i++) {
         var url = jobs[i].company_url;
         var logo = jobs[i].company_logo;
         var name = jobs[i].company;
         var title = jobs[i].title;
+        var location = jobs[i].location;
         var type = jobs[i].type;
 
-        var various_cards = createElements(url, logo, name, title, type);
+        var various_cards = createElements(url, logo, name, title, location, type);
         sectionElement.append(various_cards);
+
     }
 }
 
+function sort(sortBy) {
+    if (sortBy == 'name') {
+        sortByKey('company');
+    } else if (sortBy == 'location') {
+        sortByKey('location');
+    }
+}
+
+function sortByKey(key) {
+    jobs.sort(function(a, b) {
+        var keyOne = a[key].toLowerCase();
+        var keyTwo = b[key].toLowerCase();
+
+        if (keyOne < keyTwo) {
+            return -1;
+        }
+        if (keyOne > keyTwo) {
+            return 1;
+        }
+
+        return 0;
+    });
+    addJobDetails(jobs);
+}
 githubJobApi();
